768664a83af40d94efcdf0c20e728a8e03a29d86
Ronny Roy <royronny91@gmail.com>
2025-05-11 16:30:06 +0200

    --the learnPage is now a StatefulWidget to allow dynamic updates as the search query changes  --a fliter function by a adding of a Search bar          and a filter function for the countries, to allow users to easily find the one they're interested

--- lib/features/lernen/ui/learn_page.dart
+++ lib/features/lernen/ui/learn_page.dart
@@ -1,17 +1,54 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart'; // Provides rootBundle for loading assets
+import 'package:flutter/services.dart'; //for loading assets
 
-class LearnPage extends StatelessWidget {
- const LearnPage({super.key});
+class LearnPage extends StatefulWidget {
+  const LearnPage({super.key});
 
-  /// Asynchronously loads the JSON file containing the countries' data from the assets.
-  Future<List<dynamic>> loadCountries() async {
+  @override
+  // ignore: library_private_types_in_public_api
+  _LearnPageState createState() => _LearnPageState();
+}
+
+class _LearnPageState extends State<LearnPage> {
+  List<dynamic> allCountries = [];
+  List<dynamic> filteredCountries = [];
+  TextEditingController searchController = TextEditingController();
+
+   //loads the JSON file containing the countries' data from the assets.
+  Future<void> loadCountries() async {
     // Load the JSON content as a string from the assets directory.
     String jsonString = await rootBundle.loadString('assets/json/countries/countries.json');
     // Decode the JSON string into a List of dynamic objects.
     final List<dynamic> jsonData = jsonDecode(jsonString);
-    return jsonData;
+    jsonData.sort((a, b) => (a["name"]["common"] as String).compareTo(b["name"]["common"] as String));
+    setState(() {
+      allCountries = jsonData;
+      filteredCountries = jsonData;
+    });
+  }
+
+  void filterCountries() {
+    String query = searchController.text.toLowerCase();
+    setState(() {
+      filteredCountries = allCountries.where((country) {
+        final String name = (country["name"]["common"] as String).toLowerCase();
+        return name.contains(query);
+      }).toList();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadCountries();
+    searchController.addListener(filterCountries);
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
   }
 
   @override
@@ -22,7 +59,7 @@ class LearnPage extends StatelessWidget {
       home: Scaffold(
         body: CustomScrollView(
           slivers: [
-           // collapsible app bar integrated into the scrollable view.
+            // collapsible app bar integrated into the scrollable view.
             SliverAppBar(
               pinned: true,
               backgroundColor: Theme.of(context).primaryColor,
@@ -35,39 +72,33 @@ class LearnPage extends StatelessWidget {
                 icon: const Icon(Icons.arrow_back),
                 color: Colors.white,
                 onPressed: () {
-                  
                   Navigator.pop(context);
                 },
               ),
             ),
-            // SliverToBoxAdapter allows insertion of a normal widget into the sliver list.
             SliverToBoxAdapter(
-              child: FutureBuilder<List<dynamic>>(
-                future: loadCountries(),
-                builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
-                  // While waiting for the asynchronous call to complete, show a loading indicator.
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (snapshot.hasError) {
-                    // If an error occurred, display the error message.
-                    return Center(child: Text("Error: ${snapshot.error}"));
-                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                    // If the data is empty, display a message indicating no data was found.
-                    return const Center(child: Text("No data found"));
-                  } else {
-                    // build a ListView to display the country information.
-                    final countries = snapshot.data!;
-                    // Sort the countries alphabetically by the common name.
-                    countries.sort((a, b) => 
-                      (a["name"]["common"] as String).compareTo(b["name"]["common"] as String)
-                    );
-                    return ListView.builder(
-                      shrinkWrap: true, // Allows the ListView to size itself according to its content.
-                      // Disable internal scrolling since CustomScrollView handles scrolling.
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemCount: countries.length,
-                      itemBuilder: (context, index) {
-                        final country = countries[index];
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: searchController,
+                  decoration: InputDecoration(
+                    labelText: 'Search Countries',
+                    prefixIcon: const Icon(Icons.search),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            filteredCountries.isEmpty
+                ? const SliverToBoxAdapter(
+                    child: Center(child: CircularProgressIndicator()),
+                  )
+                : SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (BuildContext context, int index) {
+                        final country = filteredCountries[index];
                         // Extract the country's name using the JSON structure.
                         final String name = country["name"]?["common"] ?? "Unknown";
                         // Extract the capital as a list, if available.
@@ -77,20 +108,29 @@ class LearnPage extends StatelessWidget {
                             : "No capital";
                         // Get the URL for the country's flag image.
                         final String flagUrl = country["flags"]?["png"] ?? "";
-
                         return ListTile(
-                          leading: flagUrl.isNotEmpty
-                              ? Image.network(flagUrl, width: 50, height: 50)
-                              : const Icon(Icons.flag),
+                          leading: Container(
+                            width: 83,
+                            child: Row(
+                              children: [
+                                Text(
+                                  '${index + 1}.',
+                                  style: const TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                const SizedBox(width: 8),
+                                flagUrl.isNotEmpty
+                                    ? Image.network(flagUrl, width: 50, height: 50)
+                                    : const Icon(Icons.flag),
+                              ],
+                            ),
+                          ),
                           title: Text(name),
                           subtitle: Text("Capital: $capital"),
                         );
                       },
-                    );
-                  }
-                },
-              ),
-            ),
+                      childCount: filteredCountries.length,
+                    ),
+                  ),
           ],
         ),
       ),
