943afd062aa240dcbdfcc5c3a4c7795da87a27b5
AkremMek <131709527+AkremMek@users.noreply.github.com>
2025-06-03 19:12:29 +0200

    Revert "feat(auth): implement login, register and Firebase integration"

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,8 @@
 import 'package:flutter/material.dart';
-import 'app_state.dart';
 import 'features/home/ui/home_page.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'features/login/login_first_page.dart';
-import 'package:provider/provider.dart';
 
-
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(ChangeNotifierProvider(
-    create: (context) => ApplicationState(),
-    builder: ((context, child) => const MyApp()),
-  ));
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -29,30 +14,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Geo Genius',
       theme: ThemeData(
+
+
         primaryColor: const Color(0xFF00008B),
       ),
-      home: AuthGate(),
-      //home: const MyHomePage(),//(title: 'Geo Genius'),
+      home: const MyHomePage(),//(title: 'Geo Genius'),
     );
-  }
-}
-
-class AuthGate extends StatelessWidget {
-  const AuthGate ({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final appState = Provider.of<ApplicationState>(context);
-
-    if (!appState.initialized) {
-      return const Scaffold(body: Center(child: CircularProgressIndicator()));
-    }
-
-    if (appState.loggedIn) {
-      return const MyHomePage(); // Eingeloggter User
-    } else {
-      return const FirstLoginPage(); // Nicht eingeloggt
-    }
   }
 }
 
