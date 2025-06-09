aa0dc04cc18e3535c833d7dc95e5fbf11e54bde3
Fabio G <fabio.guhr@st.oth-regensburg.de>
2025-06-09 11:32:00 +0200

    Änderungen der existierenden files von der neuen UI übernommen  -Spielauswahl/ui/Widgets/flaggen_multiple_choice_button.dart -Features/home/ui/home_page.dart

--- lib/features/Spielauswahl/ui/widgets/flaggen_multiple_choice_button.dart
+++ lib/features/Spielauswahl/ui/widgets/flaggen_multiple_choice_button.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
-import '../../../Guess_the_flag_multiple_choice/ui/multiple_choice_flags_page.dart';
 
-import '../../../click_on_map_country/ui/click_on_map_country_page.dart';
 
 class MultipleChoiceFlags extends StatelessWidget{
   final String gameMode;
   const MultipleChoiceFlags({
-  Key? key,
+    super.key,
     required this.gameMode,
-}) : super(key: key);
+  });
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -19,27 +17,16 @@ class MultipleChoiceFlags extends StatelessWidget{
       elevation: 5,
       child: ListTile(
         contentPadding: const EdgeInsets.all(15),
-      title: Center(
-        child:Text(gameMode),
+        title: Center(
+          child:Text(gameMode),
+        ),
+        onTap: (){
+          //Navigator.push(context,MaterialPageRoute(builder: (context)=>FlagsMultipleChoice()),
+          // print('Flags Multiple Choice');
+
+        },
+
       ),
-      onTap: (){
-          if(gameMode == 'Guess the Flag: Multiple Choice'){
-    Navigator.push(context,MaterialPageRoute(builder: (context)=>MultipleChoiceScreen()),
-    );
-    }
-
-          else if(gameMode == 'Guess the Country via Map'){
-            Navigator.push(context,MaterialPageRoute(builder: (context)=>ClickOnMapPage()),
-    );
-
-    }
-
-
-
-
-      },
-
-    ),
     );
   }
 }
