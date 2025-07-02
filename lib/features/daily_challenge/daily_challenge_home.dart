2414c28c514110c9896db54ccc491b7754a8ba9b
Ronny Roy <royronny91@gmail.com>
2025-07-02 19:32:57 +0200

    add a inkwell-option in file lern_page.dart making the country display responsiv by displaying th details of each country

--- lib/features/daily_challenge/daily_challenge_home.dart
+++ lib/features/daily_challenge/daily_challenge_home.dart
@@ -9,6 +9,7 @@ class DailyChallengeHomePage extends StatefulWidget {
   const DailyChallengeHomePage({super.key});
 
   @override
+   // ignore: library_private_types_in_public_api
   _DailyChallengeHomePageState createState() =>
       _DailyChallengeHomePageState();
 }
